cb8b87136988bfdf79f7e1d3dd9212268499322f
Apoorv Garg <garg.apoorv1098@gmail.com>
2023-01-24 20:27:52 +0530

    Purge

--- Typescript/dir/datatype.ts
+++
@@ -1,2 +0,0 @@
-let lname = 'John';
-
--- Typescript/dir/spa.ts
+++
@@ -1,6 +0,0 @@
-// SPA - Single Page Application
-
-// Only a Single page is there and no refreshing or reload for different pages or URL.
-
-// Only hit server for data which is not available.
-
